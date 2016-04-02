f2085191c7b0481c350c38b3c161260b5071451a
Antonio Carlini <arcarlini@iee.org>
2016-04-02 17:30:47 -0600

    Provide some library support for installing debian packages.
    libs/ruby/Package.rb:
    
    Provide Package::install_apt_packages() to install debian packages via apt-install.
    Provide Package::install_dpkg_packages() to install debian packages via dpkg.
    
    libs/ruby/Package_spec.rb: Rspec tests for Package.rb

---
+++ libs/ruby/Package.rb
@@ -0,0 +1,114 @@
+#!/usr/bin/ruby -w
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require "Shell.rb"
+
+module Package
+
+  class AptOptions
+    def initialize(options)
+      reset()
+      parse(options)
+    end
+    
+    def reset()
+      @dry_run                    = false
+      @ignore_missing             = true
+      @allow_unauthenticated      = false
+      @quiet                      = false
+      @no_install_recommends      = true
+    end
+
+    def parse(options)
+      return true if options.nil?() || options.empty?()
+      # Parse each option
+      options.each() {
+        |opt|
+        case opt
+        when :dry_run                   then @dry_run = true
+        when :ignore_missing            then @ignore_missing = true
+        when :allow_unauthenticated     then @allow_unauthenticated = true
+        when :quiet                     then @quiet = true
+        when :no_install_recommends     then @no_install_recommends = true
+        else
+          return false                  # complain if an unknown option is supplied
+        end
+      }
+      return true
+    end
+
+    def text()
+      text = ""
+      text << "--dry-run "                           if @dry_run
+      text << "--ignore-missing "                    if @ignore_missing
+      text << "--allow-unauthenticated "             if @allow_unauthenticated
+      text << "--quiet "                             if @quiet
+      text << "--no-install-recommends "             if @no_install_recommends
+      return text
+    end
+  end
+
+  class DpkgOptions
+
+    def initialize(options)
+      reset()
+      parse(options)
+    end
+    
+    def reset()
+      @dry_run                    = false
+    end
+
+    def parse(options)
+      return true if options.nil?() || options.empty?()
+      # Parse each option
+      options.each() {
+        |opt|
+        case opt
+        when :dry_run                   then @dry_run = true
+        else
+          return false                  # complain if an unknown option is supplied
+        end
+      }
+      return true
+    end
+
+    def dry_run?()
+      return @dry_run
+    end
+    
+  end
+
+  def self.install_apt_packages(packages, options = nil)
+    opt = Package::AptOptions.new(options)
+    all_packages = packages.respond_to?(:each) ? packages.join(" ") : packages
+    Shell::execute_shell_commands("apt-get -y install #{opt.text()} #{all_packages}")
+  end
+
+  def self.install_dpkg_packages(packages, options)
+    opt = Package::DpkgOptions.new(options)
+    prefix = opt.dry_run?() ? "#" : ""
+    if packages.respond_to?(:each)
+      packages.each() {
+        |p|
+        Shell::execute_shell_commands("#{prefix}dpkg -i #{p}")
+      }
+    else
+      Shell::execute_shell_commands("#{prefix}dpkg -i #{packages}")
+    end
+  end
+  
+end # end of Package
+
+# Test cases
+if __FILE__ == $0
+  puts("#"*80 + "\nInstall a single package")
+  Package::install_apt_packages("ruby", [:dry_run])
+  puts("#"*80 + "\nInstall two packages")
+  Package::install_apt_packages(["ruby", "emacs"], [:dry_run])
+  puts("#"*80 + "\nInstall one dpkg")
+  Package::install_dpkg_packages("ruby", [:dry_run])
+  puts("#"*80 + "\nInstall two dpkgs")
+  Package::install_dpkg_packages(["ruby", "emacs"], [:dry_run])
+end
---
+++ libs/ruby/Package_spec.rb
@@ -0,0 +1,48 @@
+require "./Package"
+
+describe Package::Options do
+  before do
+    @opt_default = Package::Options.new(nil)
+  end
+
+  it "should NOT default to --dry-run" do
+    expect(@opt_default.text()).to_not match(/--dry-run/)
+  end
+
+  it "should default to --ignore-missing" do
+    expect(@opt_default.text()).to match(/--ignore-missing/)
+  end
+
+  it "should NOT default to --allow-unauthenticated" do
+    expect(@opt_default.text()).to_not match(/--allow-unauthenticated/)
+  end
+
+  it "should NOT default to --quite" do
+    expect(@opt_default.text()).to_not match(/--quiet/)
+  end
+
+  it "should default to --no-install-recommends" do
+    expect(@opt_default.text()).to match(/--no-install-recommends/)
+  end
+
+  it "should support :dry_run" do
+    expect(Package::Options.new([:dry_run]).text()).to match(/--dry-run/)
+  end
+
+  it "should support :ignore_missing" do
+    expect(Package::Options.new([:ignore_missing]).text()).to match(/--ignore-missing/)
+  end
+
+  it "should support :allow_unauthenticated" do
+    expect(Package::Options.new([:allow_unauthenticated]).text()).to match(/--allow-unauthenticated/)
+  end
+
+  it "should support :quiet" do
+    expect(Package::Options.new([:quiet]).text()).to match(/--quiet/)
+  end
+
+  it "should support :no_install_recommends" do
+    expect(Package::Options.new([:no_install_recommends]).text()).to match(/--no-install-recommends/)
+  end
+
+end
