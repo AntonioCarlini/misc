71f8522a07759e0c8cdca13eafc5c947cbd6deec
Antonio <arcarlini@iee.org>
2016-04-22 08:51:04 +0000

    Make nis client installation and configuration a callable ruby script.
    libs/ruby/InstallNisClient.rb: Provide install() and configure() methods.
    
    admin/debian/install-nis-client.rb: Migrate nis handling from the installer script to the callable script.

---
+++ libs/ruby/InstallNisClient.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/ruby -w
+
+require "pathname.rb"
+
+MISC_DIR = Pathname.new(__FILE__).realpath().dirname().dirname().dirname()
+$LOAD_PATH.unshift(MISC_DIR + "libs" + "ruby")
+
+require "Configuration.rb"
+require "Package.rb"
+require "Shell.rb"
+
+module InstallNisClient
+  
+  def self.install(options)
+    apt_options = []
+    apt_options << :dry_run if options.dry_run?()
+
+    # Install the nis package silently by preseeding the required values.
+    message(options, "Installing (pre-seeded) apt packages")
+    nis_domain = Configuration::get_value("nis-domain")
+    Package::install_apt_preseed_packages("nis nis/domain string #{nis_domain}", apt_options)
+  end
+  
+  def self.configure(options)
+    shell_options = []
+    shell_options << :dry_run if options.dry_run?()
+
+    # Ensure that the required domain name is set
+    nis_domain = Configuration::get_value("nis-domain")
+    Shell::execute_shell_commands("domainname #{nis_domain}", shell_options)
+  end
+
+  def self.message(options, message)
+    puts("#{File.basename(__FILE__)}: #{message}") if options.verbose?()
+  end
+
+end # end of InstallNisClient
+
+# Test cases
+if __FILE__ == $0
+  require "Installer.rb"
+  ARGV.clear()
+  ARGV << "--dry-run"
+  ARGV << "--verbose"
+  options = Installer::parse_options()
+  puts("# Install nis client (dry run, verbose)")
+  InstallNisClient::install(options)
+  puts("# Configure nis client (dry run, verbose)")
+  InstallNisClient::configure(options)
+end
