738b9c9ba4129c506f8a3df5bac6b80d73d749af
Antonio Carlini <arcarlini@iee.org>
2016-05-06 15:53:30 +0100

    libs/ruby/InstallDnsServer.rb: A script to handle instyaslling the environment for a DNS server.
    Note that no configuration as such ios currently performed by this script. It is expected that
    after installing the DNS server, a cronjob will be run regularly to update the DNS database as
    required. Once that script is finalised, perhaps scheduling it via cron will be done as part
    of the configuration here.

---
+++ libs/ruby/InstallDnsServer.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+require "pathname.rb"
+$LOAD_PATH.unshift(Pathname.new(__FILE__).realpath().dirname().dirname().dirname() + "libs" + "ruby")
+
+require "Package.rb"
+
+module InstallDnsServer
+  
+  def self.install(options)
+    apt_options = []
+    apt_options << :dry_run if options.dry_run?()
+
+    apt = []
+    apt << "bind9"
+    apt << "dnsutils"
+
+    # Install the necessary packages via apt
+    message(options, "Installing apt packages")
+    Package::install_apt_packages(apt_packages, apt_options)
+  end
+  
+  def self.configure(options)
+    shell_options = []
+    shell_options << :dry_run if options.dry_run?()
+
+  end
+
+  def self.message(options, message)
+    puts("#{File.basename(__FILE__)}: #{message}") if options.verbose?()
+  end
+
+end # end of InstallDnsServer
+
+# Test cases
+if __FILE__ == $0
+  require "Installer.rb"
+  ARGV.clear()
+  ARGV << "--dry-run"
+  ARGV << "--verbose"
+  options = Installer::parse_options()
+  puts("# Install DNS server (dry run, verbose)")
+  InstallDnsServer::install(options)
+  puts("# Configure DNS server (dry run, verbose)")
+  InstallDnsServer::configure(options)
+end
