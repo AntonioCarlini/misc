492d91ffe7737bca5e341967d8e6f2d5478049c6
Antonio Carlini <arcarlini@iee.org>
2018-05-02 21:09:48 +0000

    admin/standard-install-for-makeself.sh: First version that works for a bundle.
    The .run script built by makeself now correctly supports the variant that includes a git bundle.
    The embedded standard-install.sh script is invoked and the bunlde is made available to it.
    Command line arguments intended for standard-install.sh are correctly passed along.

--- admin/standard-install-for-makeself.sh
+++ admin/standard-install-for-makeself.sh
@@ -1,15 +1,43 @@
 #!/usr/bin/env sh
 
 # This script prepares the environment for the main ruby script.
+#
+# This script supports being invoked to install a git bundle or a tar archive.
+#
+# Parameters:
+#
+# $1
+#    bundle:  expect a bundle, invoke standard-install.sh to build a repo from it
+#    archive: unsupported at the moment
 
-echo "makeself-based installer starting ..."
+SCRIPT_DIR=`dirname 0`
 
-case $1 in
+# Try to cope if no arguments are supplied.
+# If bundle/archive is somehow missing but other args are supplied this will not work.
+# Since bundle/archive is specified at build time, this really should never happen!
+style=""
+if [ "$#" = "0" ]
+then
+    if [ -f ./misc.bundlex ]; then
+        style="bundle"
+    else
+        style="archive"
+    fi
+    echo "Installer style unknown. Guessing at '$style'"
+else
+    style=$1        # the first arg is the style (bundle or archive)
+    shift           # do not pass on the first argument
+fi
+
+# Now perform the main action.
+case $style in
     "bundle")
-	BUNDLE=${SCRIPT_DIR}/misc.bundle ${SCRIPT_DIR}/standard-install.sh
-    ;;
+	BUNDLE=${SCRIPT_DIR}/misc.bundle ${SCRIPT_DIR}/standard-install.sh "$@"
+        ;;
     "archive")
-    ;;
+        echo "Archive distribution currently unsupported."
+        exit 1
+        ;;
 esac
 
 echo "makeself-based installer finished."
